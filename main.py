9fdb8f4531dbd89088535388f8751cbe4180a84a
frcampos <42178315+frcampos@users.noreply.github.com>
2020-10-12 10:17:18 +0100

    #Adicionada a opção de fazer colocar por defeito a temperatura máxima e mínima registada e desliga temporariamente o som. Mostra ícone de satisfação se temperatura nos parâmetros

--- main.py
+++ main.py
@@ -3,35 +3,42 @@
 # Regista Numa Lista através de Buffer as leituras periódica de temperatura para posterior DownLoad.Pode ser usado para verificação das variações motivada pelo transporte.
 # É ntroduzido um valor de compensação dado que a temperatura é do CPU e não do ambiente.
 # O valor por defeito é 2ºC mas poderá ser maior ou menor
+
 def on_button_pressed_a():
     for index in range(60 * numhorasRegisto):
         basic.pause(60000 * intervaloRegisto)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def verificaAlarmetemperatura(leituraTemperatura: number):
+    if leituraTemperatura <= limSuperiorTemperatura and leituraTemperatura >= limInferiortemperatura:
+        basic.show_icon(IconNames.HAPPY)
     if leituraTemperatura > limSuperiorTemperatura:
         basic.show_icon(IconNames.HOUSE)
         basic.pause(1000)
-        music.set_volume(202)
+        music.set_volume(255)
         music.play_melody("C D E F G A B C5 ", 120)
-    else:
-        if leituraTemperatura < limInferiortemperatura:
-            basic.show_icon(IconNames.HEART)
-            basic.pause(1000)
-            music.set_volume(97)
-            music.play_melody("C5 B A G F E D C ", 120)
+    elif leituraTemperatura < limInferiortemperatura:
+        basic.show_icon(IconNames.HEART)
+        basic.pause(1000)
+        music.set_volume(1)
+        music.play_melody("C5 B A G F E D C ", 120)
+# Para que se desligue o som e
+# Para ser usado em outro transporte.
 
-def on_button_pressed_b():
+def on_button_pressed_ab():
+    global temperaturaMinimaregistada, temperaturaMaximaregistada
+    temperaturaMinimaregistada = 21
+    temperaturaMaximaregistada = 21
     music.stop_melody(MelodyStopOptions.ALL)
-input.on_button_pressed(Button.B, on_button_pressed_b)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 # Verifica se icones estão a funcionar, Se colocar a zero não testa.
 def listIcones(num: number):
     for index2 in range(num):
         basic.show_icon(IconNames.HAPPY)
-        basic.pause(1500)
+        basic.pause(500)
         basic.show_icon(IconNames.HOUSE)
-        basic.pause(1500)
+        basic.pause(500)
         basic.show_icon(IconNames.HEART)
         basic.pause(200)
     return 0
@@ -40,7 +47,8 @@ intervaloRegisto = 0
 numhorasRegisto = 0
 limSuperiorTemperatura = 0
 limInferiortemperatura = 0
-
+temperaturaMinimaregistada = 0
+temperaturaMaximaregistada = 0
 basic.clear_screen()
 temperaturaMaximaregistada = 21
 temperaturaMinimaregistada = 21
@@ -48,20 +56,20 @@ limInferiortemperatura = 20
 limSuperiorTemperatura = 25
 numhorasRegisto = 24
 intervaloRegisto = 30
-compensacaoTemperatura = 0
-basic.pause(listIcones(0))
+basic.pause(listIcones(1))
 basic.show_icon(IconNames.YES)
 
 def on_forever():
     global temperaturaReal, temperaturaMaximaregistada, temperaturaMinimaregistada
+    compensacaoTemperatura = 0
     temperaturaReal = input.temperature() - compensacaoTemperatura
     basic.show_number(temperaturaReal)
-    basic.pause(1000)
+    basic.pause(500)
     temperaturaMaximaregistada = max(temperaturaMaximaregistada, temperaturaReal)
     temperaturaMinimaregistada = min(temperaturaMinimaregistada, temperaturaReal)
     basic.show_number(temperaturaMaximaregistada)
-    basic.pause(100)
+    basic.pause(20)
     basic.show_number(temperaturaMinimaregistada)
-    basic.pause(100)
+    basic.pause(20)
     verificaAlarmetemperatura(temperaturaReal)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -9,28 +9,36 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 function verificaAlarmetemperatura (leituraTemperatura: number) {
+    if (leituraTemperatura <= limSuperiorTemperatura && leituraTemperatura >= limInferiortemperatura) {
+        basic.showIcon(IconNames.Happy)
+    }
     if (leituraTemperatura > limSuperiorTemperatura) {
         basic.showIcon(IconNames.House)
         basic.pause(1000)
-        music.setVolume(202)
+        music.setVolume(255)
         music.playMelody("C D E F G A B C5 ", 120)
     } else if (leituraTemperatura < limInferiortemperatura) {
         basic.showIcon(IconNames.Heart)
         basic.pause(1000)
-        music.setVolume(97)
+        music.setVolume(1)
         music.playMelody("C5 B A G F E D C ", 120)
     }
 }
-input.onButtonPressed(Button.B, function () {
+// Para que se desligue o som e
+// Para ser usado em outro transporte.
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+    temperaturaMinimaregistada = 21
+    temperaturaMaximaregistada = 21
     music.stopMelody(MelodyStopOptions.All)
 })
 // Verifica se icones estão a funcionar, Se colocar a zero não testa.
 function listIcones (num: number) {
     for (let index = 0; index < num; index++) {
         basic.showIcon(IconNames.Happy)
-        basic.pause(1500)
+        basic.pause(500)
         basic.showIcon(IconNames.House)
-        basic.pause(1500)
+        basic.pause(500)
         basic.showIcon(IconNames.Heart)
         basic.pause(200)
     }
@@ -41,25 +49,27 @@ let intervaloRegisto = 0
 let numhorasRegisto = 0
 let limSuperiorTemperatura = 0
 let limInferiortemperatura = 0
+let temperaturaMinimaregistada = 0
+let temperaturaMaximaregistada = 0
 basic.clearScreen()
-let temperaturaMaximaregistada = 21
-let temperaturaMinimaregistada = 21
+temperaturaMaximaregistada = 21
+temperaturaMinimaregistada = 21
 limInferiortemperatura = 20
 limSuperiorTemperatura = 25
 numhorasRegisto = 24
 intervaloRegisto = 30
-basic.pause(listIcones(0))
+basic.pause(listIcones(1))
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
     let compensacaoTemperatura = 0
     temperaturaReal = input.temperature() - compensacaoTemperatura
     basic.showNumber(temperaturaReal)
-    basic.pause(1000)
+    basic.pause(500)
     temperaturaMaximaregistada = Math.max(temperaturaMaximaregistada, temperaturaReal)
     temperaturaMinimaregistada = Math.min(temperaturaMinimaregistada, temperaturaReal)
     basic.showNumber(temperaturaMaximaregistada)
-    basic.pause(100)
+    basic.pause(20)
     basic.showNumber(temperaturaMinimaregistada)
-    basic.pause(100)
+    basic.pause(500)
     verificaAlarmetemperatura(temperaturaReal)
 })
